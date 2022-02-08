72ee724fcff5f76e43a423fb223b6f3275cd1449
adrie <adrie@192.168.1.58>
2022-02-08 15:05:34 +0100

    Spring annotation transaction

--- gestionExample/src/main/java/fr/formation/inti/controller/AppMain.java
+++ gestionExample/src/main/java/fr/formation/inti/controller/AppMain.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import fr.formation.inti.config.AppConfigurations;
 import fr.formation.inti.entity.Employee;
@@ -17,14 +19,16 @@ public class AppMain {
 	private static final Log log = LogFactory.getLog(AppMain.class);
 	
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfigurations.class);
-		
+		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfigurations.class);  //Pour les annotations
+		//ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); //pour fichier XML
+		log.info("----- getBean");
 		IEmployeeService service = context.getBean("employeeService", EmployeeService.class);
 		log.info("------ BeanService" +service);
 		List<Employee> employees = service.findAll();
-		
+		employees.forEach(log::info);
 		for(Employee e : employees)
 			log.info("------" +e);
+		
+		context.close();
 	}
-
 }
--- gestionExample/src/main/java/fr/formation/inti/service/EmployeeService.java
+++ gestionExample/src/main/java/fr/formation/inti/service/EmployeeService.java
@@ -2,26 +2,38 @@ package fr.formation.inti.service;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.formation.inti.dao.EmployeeDao;
 import fr.formation.inti.dao.IEmployeeDao;
 import fr.formation.inti.entity.Employee;
 
 @Service
+@Transactional
 public class EmployeeService implements IEmployeeService{
 	private static final Log log = LogFactory.getLog(EmployeeService.class);
 	
-	@Autowired
+	//@Autowired  //injection par setter
 	private IEmployeeDao dao;
 	
-	
+	private String message;
 	
 	public EmployeeService() {
-		log.info("new EmployeeService()");
+		log.info("------> new EmployeeService()");
+		//dao = new EmployeeDao();	
+	}
+	
+	@Autowired   //injection par constructeur
+	public EmployeeService(IEmployeeDao dao) {
+		log.info("------> new EmployeeService(dao)");
+		this.dao = dao;
 		//dao = new EmployeeDao();
 		
 	}
@@ -50,10 +62,33 @@ public class EmployeeService implements IEmployeeService{
 
 	@Override
 	public List<Employee> findAll() {
-		dao.beginTransaction();
-		List<Employee> list = dao.findAll();
-		dao.commitTransaction();
-		return list;
+		return dao.findAll();
 	}
 
+	public void setDao2(IEmployeeDao dao) {
+		log.info("------> SetDao");
+		this.dao = dao;
+	}
+
+	public void setMessage(String message) {
+		log.info(message);
+		this.message = message;
+	}
+	
+	@PostConstruct
+	private void postConstruct() {
+		log.info("-------> init service:postConstruct");
+		
+	}
+	@PreDestroy
+	private void preDestroy() {
+		log.info("-------> destroy service");
+		if(dao != null) {
+			log.info("-------> service : @PreDestroy close sessionFactory");
+		}
+	}
+	
+	
+
+	
 }
